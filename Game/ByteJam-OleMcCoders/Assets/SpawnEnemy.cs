8710f6e44519e022490929d9deef04b94625dc17
MatthewLukowski <>
2019-10-15 11:02:40 -0500

    First Commit

---
+++ Game/ByteJam-OleMcCoders/Assets/SpawnEnemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+
+    public GameObject spawnee;
+    public int spawnLimit;
+    public int spawnAmmount;
+    public bool stopSpawning = false;
+    public float spawnTime;
+    public float spawnDelay;
+
+    void Start()
+    {
+
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+
+    }
+
+    public void SpawnObject()
+    {
+
+            Instantiate(spawnee, transform.position, transform.rotation);
+            if (stopSpawning)
+            {
+                CancelInvoke("SpawnObject");
+            }
+        
+    }
+
+    void Update()
+    {
+        
+
+
+    }
+}
