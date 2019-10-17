ff9fa13bf01104658d5d6b2bd08dace053215cc7
Jake-CP <54853151+Jake-CP@users.noreply.github.com>
2019-10-17 09:39:08 -0500

    Enemy Health, Weapon Damage, Weapon Shooting

---
+++ New Unity Project/Assets/GunScript.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+
+    public Camera fpscam;
+    void Update()
+    {
+       if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+   }    
+    void Shoot()
+    {
+        RaycastHit hit;
+       if (Physics.Raycast(fpscam.transform.position, fpscam.transform.forward, out hit, range)){
+            Debug.Log(hit.transform.name);
+
+           Target target = hit.transform.GetComponent<Target>();
+           if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+
+        }
+    }
+}
