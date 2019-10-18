9f5b0a0fb21186693a3ef00d18d769da7eface70
20Cwilson <35702052+20Dcpenland@users.noreply.github.com>
2019-10-18 10:23:42 -0500

    Space X
    Spawn + VR interface now work together

---
+++ Game/ByteJam-OleMcCoders/Assets/EnemyMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+     Transform target;
+    public Transform myTrans;
+    public int Speed;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    void Update()
+    {
+
+        transform.LookAt(target);
+        transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+
+    }
+}
