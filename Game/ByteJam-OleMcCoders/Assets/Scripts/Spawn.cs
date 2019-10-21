14a5e58a106142628caca5c23231f96dff20c2cc
Mr. Wilson <bwilson@macon.k12.mo.us>
2019-10-21 10:12:47 -0500

    UpDate 10/21 from Master

---
+++ Game/ByteJam-OleMcCoders/Assets/Scripts/Spawn.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public int spawnCount;
+    public float spawnTime;
+
+    public float waveDelay;
+    public float firstWaveDelay = 0;
+    public int plusEnemies;
+    public int remainingEnemies = 0;
+
+    bool firstWave = true;
+
+    void Start()
+    {
+        StartCoroutine(SpawnObjects());
+    }
+
+    IEnumerator SpawnObjects()
+    {
+
+        // Initial wait
+
+        if (firstWave = true)
+        {
+            yield return new WaitForSeconds(firstWaveDelay);
+            
+            firstWave = false;
+        }
+        else { yield return new WaitForSeconds(waveDelay); }
+
+
+        yield return new WaitForSeconds(waveDelay);
+        
+        
+        
+        for (int count = spawnCount; count > 0; --count)
+        {
+            GameObject clone = Instantiate(prefab, transform.position, transform.rotation);
+
+            DestroyEventEmitter destroyEventEmitter = clone.AddComponent<DestroyEventEmitter>();
+            destroyEventEmitter.OnObjectDestroyedEvent += OnGameObjectDestroyed;
+            remainingEnemies++;
+
+            // Wait before next spawn
+            yield return new WaitForSeconds(spawnTime);
+
+        }
+
+        Debug.Log("All the ennemies have been instantiated !");
+    }
+
+    private void OnGameObjectDestroyed(DestroyEventEmitter emitter)
+    {
+        remainingEnemies--;
+        emitter.OnObjectDestroyedEvent -= OnGameObjectDestroyed;
+
+        Debug.Log("Remaining ennemies : " + remainingEnemies);
+
+        if (remainingEnemies == 0)
+        {
+            spawnCount = spawnCount+ plusEnemies;
+
+            StartCoroutine(SpawnObjects());
+        }
+    }
+}
