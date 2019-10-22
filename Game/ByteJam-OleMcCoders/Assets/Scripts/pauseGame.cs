213161b83646cdc70d92481617b6743912f2a03d
MatthewLukowski <>
2019-10-22 08:25:27 -0500

    Random spawn positions

---
+++ Game/ByteJam-OleMcCoders/Assets/Scripts/pauseGame.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseGame : MonoBehaviour
+{
+    public bool gamePaused = false;
+    public GameObject pauseMenu;
+
+    void Start()
+    {
+
+        if (gamePaused == false)
+        {
+            Time.timeScale = 0;
+            gamePaused = true;
+            // Cursor.visible = true;
+            pauseMenu.SetActive(true);
+        }
+        else
+        {
+            pauseMenu.SetActive(false);
+            //  Cursor.visible = false;
+            gamePaused = false;
+            Time.timeScale = 1;
+
+        }
+
+    }
+
+
+      public  void Pause() {
+            Time.timeScale = 0;
+            gamePaused = true;
+            // Cursor.visible = true;
+            pauseMenu.SetActive(true);
+        }
+
+       public void unpause()
+        {
+            pauseMenu.SetActive(false);
+            //  Cursor.visible = false;
+            gamePaused = false;
+            Time.timeScale = 1;
+        }
+    }
+
